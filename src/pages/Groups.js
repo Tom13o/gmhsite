7650b7f1b0bc2fd8fdc3526ccb35deaff390b3c8
Tom <90448210+Tom13o@users.noreply.github.com>
2022-10-21 10:58:29 +1100

    started group code

--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { db } from "../index";
+import { collection, addDoc } from "firebase/firestore";
+import { AuthContext } from "../auth";
 
 export default function Groups() {
+    const { currentUser } = useContext(AuthContext);
+    const handleGroupCreation = async event => {
+        event.preventDefault()
+        const { groupname } = event.target.elements;
+        try {
+            const groupRef = await addDoc(collection(db, "groups"), {
+                name: groupname.value,
+                members: [{
+                    id: currentUser.uid,
+                    // joinDate: ,
+                    status: {},
+                    pastStatuses: [],
+                    owner: true
+                }]
+            });
+            console.log("Group made with ID: ", groupRef.id);
+            // add group to user group list
+        } catch (error) {
+            alert(error);
+        }
+    }
     // literally every line of this disgusting code needs to be changed
     // this is an awful implementation but i'm using it to test the db rather than the UI
     // in future (near future please) use React state to show modal
@@ -8,7 +32,10 @@ export default function Groups() {
     return(
         <>
             <div className="addGroupModal" style="display: none">
-                add group
+                <form onSubmit={handleGroupCreation}>
+                    <input name="groupname" type="text" placeholder="Group Name" />
+                    <button type="submit">Create Group</button>
+                </form>
             </div>
             Groups
             <input type="button" onClick={document.getElementsByClassName(addGroupModal)[0].style.display = "block"}>Create New Group</input>
