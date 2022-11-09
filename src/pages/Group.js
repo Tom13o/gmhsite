c6939b3ca44d42589787c2406683eb1f56ee78d4
Tom <90448210+Tom13o@users.noreply.github.com>
2022-11-10 09:29:26 +1100

    i have no idea what im doing

--- src/pages/Group.js
+++ src/pages/Group.js
@@ -67,7 +67,7 @@ export default function Group() {
                     if (isToday(Date(member["statuses"][member["statuses"].length-1]["date"]))) {
                         alert("Most recent status was today.")
                     } else {
-                        const str = "members." + i + ".statuses"
+                        const str = "members[" + i + "].statuses";
                         await updateDoc(doc(db, "groups", chosenGroup), {
                             [str]: arrayUnion({
                                 feeling: feeling,
